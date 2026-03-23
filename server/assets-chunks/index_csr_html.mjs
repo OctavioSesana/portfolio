128245eb69b3a4b9e5c04bcfa2c8b9d8102f3e0c
OctavioSesana <osesana@frro.utn.edu.ar>
2026-03-23 16:03:11 -0300

    Auto-generated commit

---
+++ server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,17 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>PortfolioOctavio</title>
+  <base href="/portfolio/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%233b82f6' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpolyline points='16 18 22 12 16 6'%3E%3C/polyline%3E%3Cpolyline points='8 6 2 12 8 18'%3E%3C/polyline%3E%3C/svg%3E">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="styles-5INURTSO.css"></head>
+<body ngcm="">
+  <app-root></app-root>
+<script src="main-MHE4L4OL.js" type="module"></script></body>
+</html>
+`;
